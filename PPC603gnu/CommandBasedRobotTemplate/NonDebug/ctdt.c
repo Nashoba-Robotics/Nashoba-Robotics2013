bf3474bc19e645e3be90eb0447f974b1c4513c2c
Colin <colinw@savix.net>
2013-10-18 19:18:45 -0400

    Updating

---
+++ PPC603gnu/CommandBasedRobotTemplate/NonDebug/ctdt.c
@@ -0,0 +1,359 @@
+/* ctors and dtors arrays -- to be used by runtime system */
+/*   to call static constructors and destructors          */
+/*                                                        */
+/* NOTE: Use a C compiler to compile this file. If you    */
+/*       are using GNU C++, be sure to use compile this   */
+/*       file using a GNU compiler with the               */
+/*       -fdollars-in-identifiers flag.                   */
+
+
+void _GLOBAL__I__ZN16TrackIdleCommandC2Ev();
+
+void _GLOBAL__I__ZN17AutonomousCommandC2Ev();
+
+void _GLOBAL__I__ZN17CoprocessorVisionC2Ev();
+
+void _GLOBAL__I__ZN17DriveBrakeCommandC2Ev();
+
+void _GLOBAL__I__ZN17DriveDriveCommandC2Ev();
+
+void _GLOBAL__I__ZN17ShooterRunCommandC2Ev();
+
+void _GLOBAL__I__ZN18DriveRotateCommandC2Eff();
+
+void _GLOBAL__I__ZN18ShooterIdleCommandC2Ev();
+
+void _GLOBAL__I__ZN18ShooterLoadCommandC2Ev();
+
+void _GLOBAL__I__ZN18ShooterStopCommandC2Ev();
+
+void _GLOBAL__I__ZN18TippingIdleCommandC2Ev();
+
+void _GLOBAL__I__ZN20PneumaticsCompressorC2Ev();
+
+void _GLOBAL__I__ZN20TippingDeployCommandC2Ev();
+
+void _GLOBAL__I__ZN21CompressorIdleCommandC2Ev();
+
+void _GLOBAL__I__ZN21CompressorStopCommandC2Ev();
+
+void _GLOBAL__I__ZN21DriveDirectionCommandC2Efff();
+
+void _GLOBAL__I__ZN21ShootFourTimesCommandC2Ev();
+
+void _GLOBAL__I__ZN22CheckAllSensorsCommandC2Ev();
+
+void _GLOBAL__I__ZN22CompressorStartCommandC2Ev();
+
+void _GLOBAL__I__ZN22TippingUndeployCommandC2Ev();
+
+void _GLOBAL__I__ZN22TrackDriveSpeedCommandC2Ef();
+
+void _GLOBAL__I__ZN23TrackDeployBrakeCommandC2Ev();
+
+void _GLOBAL__I__ZN25TrackUndeployBrakeCommandC2Ev();
+
+void _GLOBAL__I__ZN2OIC2Ev();
+
+void _GLOBAL__I__ZN31TrackDriveDownUntilForceCommandC2Ev();
+
+void _GLOBAL__I__ZN32CompressorAutomaticToggleCommandC2Ev();
+
+void _GLOBAL__I__ZN5DriveC2Ev();
+
+void _GLOBAL__I__ZN5Robot5driveE();
+
+void _GLOBAL__I__ZN5TrackC2Ev();
+
+void _GLOBAL__I__ZN7ShooterC2Ev();
+
+void _GLOBAL__I__ZN7TippingC2Ev();
+
+void _GLOBAL__I__ZN8RobotMap15compressorRelayE();
+
+void _GLOBAL__I_pidcoefs();
+
+void _GLOBAL__I_pidcoefsPole();
+
+void _GLOBAL__I__ZN12PrintCommandC2EPKc();
+
+void _GLOBAL__I__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__I__ZN11WaitCommandC2Ed();
+
+void _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__I__ZN10LiveWindow10m_instanceE();
+
+void _GLOBAL__I__ZN17MotorSafetyHelper12m_headHelperE();
+
+void _GLOBAL__I__ZN8Notifier21kTimerInterruptNumberE();
+
+void _GLOBAL__I__ZN8Resource12m_createLockE();
+
+void _GLOBAL__I__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__I__ZN14SmartDashboard7m_tableE();
+
+void _GLOBAL__I__ZN12SolenoidBase11m_semaphoreE();
+
+void _GLOBAL__I__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__I_AxisCamera_debugFlag();
+
+void _GLOBAL__I__ZN12NetworkTable19PATH_SEPARATOR_CHARE();
+
+void _GLOBAL__I__ZN16NetworkTableMode6ServerE();
+
+void _GLOBAL__I__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager();
+
+void _GLOBAL__I_OutgoingEntryReceiver_NULL();
+
+void _GLOBAL__I__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem();
+
+void _GLOBAL__I__ZN21ClientConnectionState24DISCONNECTED_FROM_SERVERE();
+
+void _GLOBAL__I__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager();
+
+void _GLOBAL__I__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE();
+
+void _GLOBAL__I__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE();
+
+void _GLOBAL__I__ZN17DefaultEntryTypes7BOOLEANE();
+
+void _GLOBAL__I__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE();
+
+void _GLOBAL__I__ZN11StringArray19STRING_ARRAY_RAW_IDE();
+
+extern void (*_ctors[])();
+void (*_ctors[])() =
+    {
+    _GLOBAL__I__ZN16TrackIdleCommandC2Ev,
+    _GLOBAL__I__ZN17AutonomousCommandC2Ev,
+    _GLOBAL__I__ZN17CoprocessorVisionC2Ev,
+    _GLOBAL__I__ZN17DriveBrakeCommandC2Ev,
+    _GLOBAL__I__ZN17DriveDriveCommandC2Ev,
+    _GLOBAL__I__ZN17ShooterRunCommandC2Ev,
+    _GLOBAL__I__ZN18DriveRotateCommandC2Eff,
+    _GLOBAL__I__ZN18ShooterIdleCommandC2Ev,
+    _GLOBAL__I__ZN18ShooterLoadCommandC2Ev,
+    _GLOBAL__I__ZN18ShooterStopCommandC2Ev,
+    _GLOBAL__I__ZN18TippingIdleCommandC2Ev,
+    _GLOBAL__I__ZN20PneumaticsCompressorC2Ev,
+    _GLOBAL__I__ZN20TippingDeployCommandC2Ev,
+    _GLOBAL__I__ZN21CompressorIdleCommandC2Ev,
+    _GLOBAL__I__ZN21CompressorStopCommandC2Ev,
+    _GLOBAL__I__ZN21DriveDirectionCommandC2Efff,
+    _GLOBAL__I__ZN21ShootFourTimesCommandC2Ev,
+    _GLOBAL__I__ZN22CheckAllSensorsCommandC2Ev,
+    _GLOBAL__I__ZN22CompressorStartCommandC2Ev,
+    _GLOBAL__I__ZN22TippingUndeployCommandC2Ev,
+    _GLOBAL__I__ZN22TrackDriveSpeedCommandC2Ef,
+    _GLOBAL__I__ZN23TrackDeployBrakeCommandC2Ev,
+    _GLOBAL__I__ZN25TrackUndeployBrakeCommandC2Ev,
+    _GLOBAL__I__ZN2OIC2Ev,
+    _GLOBAL__I__ZN31TrackDriveDownUntilForceCommandC2Ev,
+    _GLOBAL__I__ZN32CompressorAutomaticToggleCommandC2Ev,
+    _GLOBAL__I__ZN5DriveC2Ev,
+    _GLOBAL__I__ZN5Robot5driveE,
+    _GLOBAL__I__ZN5TrackC2Ev,
+    _GLOBAL__I__ZN7ShooterC2Ev,
+    _GLOBAL__I__ZN7TippingC2Ev,
+    _GLOBAL__I__ZN8RobotMap15compressorRelayE,
+    _GLOBAL__I_pidcoefs,
+    _GLOBAL__I_pidcoefsPole,
+    _GLOBAL__I__ZN12PrintCommandC2EPKc,
+    _GLOBAL__I__ZN9Scheduler9_instanceE,
+    _GLOBAL__I__ZN11WaitCommandC2Ed,
+    _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__I__ZN10LiveWindow10m_instanceE,
+    _GLOBAL__I__ZN17MotorSafetyHelper12m_headHelperE,
+    _GLOBAL__I__ZN8Notifier21kTimerInterruptNumberE,
+    _GLOBAL__I__ZN8Resource12m_createLockE,
+    _GLOBAL__I__ZN9RobotBase10m_instanceE,
+    _GLOBAL__I__ZN14SmartDashboard7m_tableE,
+    _GLOBAL__I__ZN12SolenoidBase11m_semaphoreE,
+    _GLOBAL__I__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__I_AxisCamera_debugFlag,
+    _GLOBAL__I__ZN12NetworkTable19PATH_SEPARATOR_CHARE,
+    _GLOBAL__I__ZN16NetworkTableMode6ServerE,
+    _GLOBAL__I__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager,
+    _GLOBAL__I_OutgoingEntryReceiver_NULL,
+    _GLOBAL__I__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem,
+    _GLOBAL__I__ZN21ClientConnectionState24DISCONNECTED_FROM_SERVERE,
+    _GLOBAL__I__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager,
+    _GLOBAL__I__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE,
+    _GLOBAL__I__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE,
+    _GLOBAL__I__ZN17DefaultEntryTypes7BOOLEANE,
+    _GLOBAL__I__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE,
+    _GLOBAL__I__ZN11StringArray19STRING_ARRAY_RAW_IDE,
+    0
+    };
+
+void _GLOBAL__D__ZN16TrackIdleCommandC2Ev();
+
+void _GLOBAL__D__ZN17AutonomousCommandC2Ev();
+
+void _GLOBAL__D__ZN17CoprocessorVisionC2Ev();
+
+void _GLOBAL__D__ZN17DriveBrakeCommandC2Ev();
+
+void _GLOBAL__D__ZN17DriveDriveCommandC2Ev();
+
+void _GLOBAL__D__ZN17ShooterRunCommandC2Ev();
+
+void _GLOBAL__D__ZN18DriveRotateCommandC2Eff();
+
+void _GLOBAL__D__ZN18ShooterIdleCommandC2Ev();
+
+void _GLOBAL__D__ZN18ShooterLoadCommandC2Ev();
+
+void _GLOBAL__D__ZN18ShooterStopCommandC2Ev();
+
+void _GLOBAL__D__ZN18TippingIdleCommandC2Ev();
+
+void _GLOBAL__D__ZN20PneumaticsCompressorC2Ev();
+
+void _GLOBAL__D__ZN20TippingDeployCommandC2Ev();
+
+void _GLOBAL__D__ZN21CompressorIdleCommandC2Ev();
+
+void _GLOBAL__D__ZN21CompressorStopCommandC2Ev();
+
+void _GLOBAL__D__ZN21DriveDirectionCommandC2Efff();
+
+void _GLOBAL__D__ZN21ShootFourTimesCommandC2Ev();
+
+void _GLOBAL__D__ZN22CheckAllSensorsCommandC2Ev();
+
+void _GLOBAL__D__ZN22CompressorStartCommandC2Ev();
+
+void _GLOBAL__D__ZN22TippingUndeployCommandC2Ev();
+
+void _GLOBAL__D__ZN22TrackDriveSpeedCommandC2Ef();
+
+void _GLOBAL__D__ZN23TrackDeployBrakeCommandC2Ev();
+
+void _GLOBAL__D__ZN25TrackUndeployBrakeCommandC2Ev();
+
+void _GLOBAL__D__ZN2OIC2Ev();
+
+void _GLOBAL__D__ZN31TrackDriveDownUntilForceCommandC2Ev();
+
+void _GLOBAL__D__ZN32CompressorAutomaticToggleCommandC2Ev();
+
+void _GLOBAL__D__ZN5DriveC2Ev();
+
+void _GLOBAL__D__ZN5Robot5driveE();
+
+void _GLOBAL__D__ZN5TrackC2Ev();
+
+void _GLOBAL__D__ZN7ShooterC2Ev();
+
+void _GLOBAL__D__ZN7TippingC2Ev();
+
+void _GLOBAL__D__ZN8RobotMap15compressorRelayE();
+
+void _GLOBAL__D_pidcoefs();
+
+void _GLOBAL__D_pidcoefsPole();
+
+void _GLOBAL__D__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__D__ZN17MotorSafetyHelper12m_headHelperE();
+
+void _GLOBAL__D__ZN8Notifier21kTimerInterruptNumberE();
+
+void _GLOBAL__D__ZN8Resource12m_createLockE();
+
+void _GLOBAL__D__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__D__ZN14SmartDashboard7m_tableE();
+
+void _GLOBAL__D__ZN12SolenoidBase11m_semaphoreE();
+
+void _GLOBAL__D__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__D_AxisCamera_debugFlag();
+
+void _GLOBAL__D__ZN12NetworkTable19PATH_SEPARATOR_CHARE();
+
+void _GLOBAL__D__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager();
+
+void _GLOBAL__D_OutgoingEntryReceiver_NULL();
+
+void _GLOBAL__D__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem();
+
+void _GLOBAL__D__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager();
+
+void _GLOBAL__D__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE();
+
+void _GLOBAL__D__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE();
+
+void _GLOBAL__D__ZN17DefaultEntryTypes7BOOLEANE();
+
+void _GLOBAL__D__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE();
+
+void _GLOBAL__D__ZN11StringArray19STRING_ARRAY_RAW_IDE();
+
+extern void (*_dtors[])();
+void (*_dtors[])() =
+    {
+    _GLOBAL__D__ZN16TrackIdleCommandC2Ev,
+    _GLOBAL__D__ZN17AutonomousCommandC2Ev,
+    _GLOBAL__D__ZN17CoprocessorVisionC2Ev,
+    _GLOBAL__D__ZN17DriveBrakeCommandC2Ev,
+    _GLOBAL__D__ZN17DriveDriveCommandC2Ev,
+    _GLOBAL__D__ZN17ShooterRunCommandC2Ev,
+    _GLOBAL__D__ZN18DriveRotateCommandC2Eff,
+    _GLOBAL__D__ZN18ShooterIdleCommandC2Ev,
+    _GLOBAL__D__ZN18ShooterLoadCommandC2Ev,
+    _GLOBAL__D__ZN18ShooterStopCommandC2Ev,
+    _GLOBAL__D__ZN18TippingIdleCommandC2Ev,
+    _GLOBAL__D__ZN20PneumaticsCompressorC2Ev,
+    _GLOBAL__D__ZN20TippingDeployCommandC2Ev,
+    _GLOBAL__D__ZN21CompressorIdleCommandC2Ev,
+    _GLOBAL__D__ZN21CompressorStopCommandC2Ev,
+    _GLOBAL__D__ZN21DriveDirectionCommandC2Efff,
+    _GLOBAL__D__ZN21ShootFourTimesCommandC2Ev,
+    _GLOBAL__D__ZN22CheckAllSensorsCommandC2Ev,
+    _GLOBAL__D__ZN22CompressorStartCommandC2Ev,
+    _GLOBAL__D__ZN22TippingUndeployCommandC2Ev,
+    _GLOBAL__D__ZN22TrackDriveSpeedCommandC2Ef,
+    _GLOBAL__D__ZN23TrackDeployBrakeCommandC2Ev,
+    _GLOBAL__D__ZN25TrackUndeployBrakeCommandC2Ev,
+    _GLOBAL__D__ZN2OIC2Ev,
+    _GLOBAL__D__ZN31TrackDriveDownUntilForceCommandC2Ev,
+    _GLOBAL__D__ZN32CompressorAutomaticToggleCommandC2Ev,
+    _GLOBAL__D__ZN5DriveC2Ev,
+    _GLOBAL__D__ZN5Robot5driveE,
+    _GLOBAL__D__ZN5TrackC2Ev,
+    _GLOBAL__D__ZN7ShooterC2Ev,
+    _GLOBAL__D__ZN7TippingC2Ev,
+    _GLOBAL__D__ZN8RobotMap15compressorRelayE,
+    _GLOBAL__D_pidcoefs,
+    _GLOBAL__D_pidcoefsPole,
+    _GLOBAL__D__ZN9Scheduler9_instanceE,
+    _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__D__ZN17MotorSafetyHelper12m_headHelperE,
+    _GLOBAL__D__ZN8Notifier21kTimerInterruptNumberE,
+    _GLOBAL__D__ZN8Resource12m_createLockE,
+    _GLOBAL__D__ZN9RobotBase10m_instanceE,
+    _GLOBAL__D__ZN14SmartDashboard7m_tableE,
+    _GLOBAL__D__ZN12SolenoidBase11m_semaphoreE,
+    _GLOBAL__D__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__D_AxisCamera_debugFlag,
+    _GLOBAL__D__ZN12NetworkTable19PATH_SEPARATOR_CHARE,
+    _GLOBAL__D__ZN30AbstractNetworkTableEntryStoreC2ER20TableListenerManager,
+    _GLOBAL__D_OutgoingEntryReceiver_NULL,
+    _GLOBAL__D__ZN12WriteManagerC2ER30FlushableOutgoingEntryReceiverR15NTThreadManagerR30AbstractNetworkTableEntryStorem,
+    _GLOBAL__D__ZN18NetworkTableServerC2ER16IOStreamProviderR28NetworkTableEntryTypeManagerR15NTThreadManager,
+    _GLOBAL__D__ZN21ServerConnectionState26GOT_CONNECTION_FROM_CLIENTE,
+    _GLOBAL__D__ZN12BooleanArray20BOOLEAN_ARRAY_RAW_IDE,
+    _GLOBAL__D__ZN17DefaultEntryTypes7BOOLEANE,
+    _GLOBAL__D__ZN11NumberArray19NUMBER_ARRAY_RAW_IDE,
+    _GLOBAL__D__ZN11StringArray19STRING_ARRAY_RAW_IDE,
+    0
+    };
